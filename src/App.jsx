94d2b7fcabab31a219f1140149a2b5dad38cd889
flyonskncn <yuzhix4477@126.com>
2024-06-01 06:58:42 -0400

    Updating the app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,16 @@
 import React from 'react';
 import Header from './components/Header';
-import Main from './components/Adminpage';
+import AdminPage from './Pages/Adminpage';
 import Footer from './components/Footer';
 
 function App() {
   return (
     <div className="min-h-screen bg-gray-50">
       <Header />
-      <Main />
+      <AdminPage />
       <Footer />
     </div>
   );
 }
 
 export default App;
-
